f297a37363c584c62cb79cb909d5a04d5480c4b0
Haliima Ahmed <43786387+HaliimaAhmed@users.noreply.github.com>
2020-12-27 22:49:26 -0500

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,22 @@
+/* PROGRAM:  hello.c 
+   AUTHOR:   Halima Ahmed
+   TOPIC:    Basic Programming 
+   PURPOSE:  Introduction to C language
+   NOTES:    hello program
+
+*/
+
+/**************************************************************************/
+/* include standard files
+ **************************************************************************/
+#include <stdio.h>
+
+/**************************************************************************
+ * Main
+ **************************************************************************/
+int main( void ) {
+
+	printf( "Hello & Welcome to C-Language repository!\n" );
+
+	return 0;
+}
